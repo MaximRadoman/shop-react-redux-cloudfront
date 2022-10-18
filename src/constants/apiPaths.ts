777e69f76d84edeac8acdb455cb35541e3aaa0a2
Radaman, Maksim <mradaman@estee.com>
2022-10-18 13:03:23 +0200

    Change bff

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://khijagt6v9.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://11d4n98czg.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
